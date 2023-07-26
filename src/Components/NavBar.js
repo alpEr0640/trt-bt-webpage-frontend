be4d62309149c8cba58e834506cdcf2b8a70035a
qenane <s200444059@stu.thk.edu.tr>
2023-07-26 17:38:39 +0300

    Renkler ayarlandı font düzenlendi

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,59 +1,66 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
-import logo from "../images/trt.png"
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import '../CssPages/NavBar.css'
+import logo from"../images/trt.png"
 
 function NavBar() {
   return (
-    <div style={{height:"1000px"}}>
+    <div style={{height:"10000px"}}>
       
 
-    <nav style={{position:"fixed",borderRadius:"12px"}} className="navbar navbar-expand-lg fixed-top navbar-light  shadow p-1  m-4">
-      <div   classNameName="logo container">
-        <a className="navbar-brand" href="#">
-        <img src={logo} alt="trtLogo" style={{height: '100%'}} />
-
-        </a>
+    <nav style={ {position:"fixed" ,borderRadius:"12px", height:"44px"}} className="navbar navbar-expand-lg fixed-top navbar-light text-dark bg-body-teriarty shadow p-1 m-4">
+      <div   classNameName="container">
       </div>
+        <a className="navbar-brand mx-4" href="#">
+        <img src={logo} alt="Logo"  height={"24"} />
+        </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+        
       <div
          className="container-fluid "
-         id="navbarsExample08"
+         id="navbarText"
          >
-        <ul className="nav nav-pills nav-fill ">
+        <ul className="collapse navbar-collapse navbar-nav me-auto mb-2 mb-lg-0 nav nav-pills nav-justified mx-5 ">
           <li className="nav-item mx-4 ">
-            <a className="flex-sm-fill text-sm-center nav-link active" style={{borderRadius:"12px"}} aria-current="page" href="#">
+            <a className="nav-link active font-weight-bold" style={{borderRadius:"12px"}} aria-current="page" href="#">
               Ana Sayfa
             </a>
           </li>
-          <li className="nav-item mx-4">
-            <a className="flex-sm-fill text-sm-center nav-link  " style={{borderRadius:"12px"}} href="#">
+          <li className="nav-item mx-4  kenan">
+            <a className="nav-link  kenan " style={{borderRadius:"12px"}} href="#">
               Çalışma Arkadaşlarımız
             </a>
           </li>
-          <li className="nav-item mx-4">
-            <a className="flex-sm-fill text-sm-center nav-link " style={{borderRadius:"12px"}} href="#">
+          <li className="nav-item mx-4 ">
+            <a className="nav-link kenan" style={{borderRadius:"12px"}} href="#">
               Uygulamalar
             </a>
           </li>
           <li className="nav-item mx-4">
-            <a className="flex-sm-fill text-sm-center nav-link " style={{borderRadius:"12px"}} href="#">
+            <a className="nav-link kenan" style={{borderRadius:"12px"}} href="#">
               Mevzuat
             </a>
           </li>
           <li className="nav-item mx-4">
-            <a className="flex-sm-fill text-sm-center nav-link " style={{borderRadius:"12px"}} href="#">
+            <a className="nav-link kenan" style={{borderRadius:"12px"}} href="#">
               Kılavuzlar
             </a>
           </li>
           <li className="nav-item mx-4">
-            <a className="flex-sm-fill text-sm-center nav-link " style={{borderRadius:"12px"}} href="#">
+            <a className="nav-link kenan" style={{borderRadius:"12px"}} href="#">
               Forum
             </a>
           </li>
         </ul>
+        <span class="navbar-text">
+      </span>
       </div>
-      <div classNameName="login ">
-        <button style={{borderRadius:"12px"}} className="btn btn-danger mx-4">GİRİŞ</button>
-      </div>
+        <div className="login ">
+          <button style={{width:"100px" ,borderRadius:"12px"}} className="btn active mx-4 font-weight-bold">GİRİŞ</button>
+        </div>
     </nav>
           </div>
   );
