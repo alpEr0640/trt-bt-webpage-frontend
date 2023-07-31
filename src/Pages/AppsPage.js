7a2e79b98c86f21c51d903f1de2a170c6397d808
Muhammet Akif Ayan <akifayan98@gmail.com>
2023-07-31 17:04:21 +0300

    commit deneme

---
+++ src/Pages/AppsPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../Components/NavBar";
+
+function AppsPage() {
+  return (
+    <div>
+      <NavBar />
+      <Applications/>
+      
+    </div>
+  );
+}
+
+export default AppsPage;
