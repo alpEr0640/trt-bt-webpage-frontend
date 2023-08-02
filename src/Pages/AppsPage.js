7e4b2acfa369a6b13201d82a9322c1c7154113c1
Muhammet Akif Ayan <akifayan98@gmail.com>
2023-08-02 15:05:43 +0300

    finish

--- src/Pages/AppsPage.js
+++ src/Pages/AppsPage.js
@@ -1,11 +1,12 @@
 import React from "react";
 import NavBar from "../Components/NavBar";
+import Applications from "../Components/Applications";
 
 function AppsPage() {
   return (
     <div>
       <NavBar />
-      <Applications/>
+      <Applicationsq1/>
       
     </div>
   );
